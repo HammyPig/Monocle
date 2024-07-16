d4f826a947adfb387b544227aab4d9d4b661bfdd
HammyPig <james.bruce.walden@gmail.com>
2024-07-16 23:08:08 +1000

    add capital gains tax discount consideration

--- src/monocle/tax.py
+++ src/monocle/tax.py
@@ -1,6 +1,9 @@
+from dateutil.relativedelta import relativedelta
+
 class Tax:
     class ShareParcel:
-        def __init__(self, units, cost_base):
+        def __init__(self, date_bought, units, cost_base):
+            self.date_bought = date_bought
             self.units = units
             self.cost_base = cost_base
 
@@ -12,7 +15,7 @@ class Tax:
             if t.action == "buy":
                 if t.stock not in share_parcels: share_parcels[t.stock] = []
                 cost_base = t.amount / t.units
-                share_parcel = Tax.ShareParcel(t.units, cost_base)
+                share_parcel = Tax.ShareParcel(t.date, t.units, cost_base)
                 share_parcels[t.stock].insert(0, share_parcel)
             elif t.action == "sell":
                 capital_proceeds = t.amount / t.units
@@ -23,7 +26,12 @@ class Tax:
                     shares_sold_from_parcel = min(sold_units, share_parcel.units)
 
                     if t.date >= start_date and t.date <= end_date:
-                        capital_gains += shares_sold_from_parcel * (capital_proceeds - share_parcel.cost_base)
+                        capital_gain = shares_sold_from_parcel * (capital_proceeds - share_parcel.cost_base)
+                        
+                        if capital_gain > 0 and t.date > share_parcel.date_bought + relativedelta(months=12):
+                            capital_gain /= 2
+
+                        capital_gains += capital_gain
 
                     share_parcels[t.stock][i].units -= shares_sold_from_parcel
                     if share_parcels[t.stock][i].units <= 0:
